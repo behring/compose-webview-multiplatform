045d09eba1f46c03141443425e0c8229620fb69c
Lin.Zhao <lizhao@thoughtworks.com>
2025-04-24 10:16:22 +0800

    bugfix: Dispatchers.Main is a dedicated scheduler for Android provided by kotlinx - coroutines - android. However, it may cause an android.os.Looper error. Therefore, replace Dispatchers.Main with Dispatchers.Default to be compatible with Android, iOS, and Desktop.

--- webview/src/commonMain/kotlin/com/multiplatform/webview/web/WebViewNavigator.kt
+++ webview/src/commonMain/kotlin/com/multiplatform/webview/web/WebViewNavigator.kt
@@ -123,10 +123,10 @@ class WebViewNavigator(
     /**
      * Handles navigation events from the composable and calls the appropriate method on the
      * [IWebView].
-     * Use Dispatchers.Main to ensure that the webview methods are called on UI thread
+     * Use Dispatchers.Default to ensure that the webview methods are called on UI thread
      */
     internal suspend fun IWebView.handleNavigationEvents(): Nothing =
-        withContext(Dispatchers.Main) {
+        withContext(Dispatchers.Default) {
             navigationEvents.collect { event ->
                 KLogger.d {
                     "WebViewNavigator handleNavigationEvents: $event"
